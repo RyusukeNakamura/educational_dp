169cc0883bcc3dfbfa011ab9fcf09a9612901d9e
RyusukeNakamura <leiden3535@gmail.com>
2020-12-29 15:11:51 +0900

    speed up F by cumulative maximum

--- F.py
+++ F.py
@@ -1,15 +1,17 @@
-s = input()
-t = input()
+import numpy as np
+
+s = np.array(list(input()))
+t = np.array(list(input()))
+equal = (s[:, None] == t[None, :]) # 対応表の作成
+
 slen, tlen = len(s), len(t)
 dp = [[0 for j in range(tlen+1)] for i in range(slen+1)]
 
 # dp[i][j]...sをi-1文字目, tをj-1文字目まで見たときの最長共通部分の長さ
-for i, si in enumerate(s):
-    for j, tj in enumerate(t):
-        if si == tj:
-            dp[i+1][j+1] = dp[i][j] + 1
-        else:
-            dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+for i in range(slen):
+    dp[i+1][1:] = np.maximum(dp[i][:-1] + equal[i], dp[i][1:]) # 共通文字発見時，加えた方が大きくなるかどうか
+    dp[i+1] = np.maximum.accumulate(dp[i+1]) # 累積最大値
+
 
 l = dp[slen][tlen]
 ans = ['' for _ in range(l)]
