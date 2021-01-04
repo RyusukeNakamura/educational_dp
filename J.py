f4d4c82bc3df57a275668bb51518836f6a224943
RyusukeNakamura <leiden3535@gmail.com>
2021-01-04 17:28:43 +0900

    J, N^3 for. pypy

--- J.py
+++ J.py
@@ -1,3 +1,4 @@
+# PyPy3だとAC．
 N = int(input())
 A = list(map(int, input().split()))
 one, two, three = 0, 0, 0
@@ -9,25 +10,23 @@ for a in A:
     else:
         three += 1
 
-dp = [[[-1 for k in range(N+1)] for j in range(N+1)] for i in range(N+1)]
+dp = [[[0 for k in range(N+1)] for j in range(N+1)] for i in range(N+1)]
 
-def rec(i, j, k):
-    if dp[i][j][k] >= 0:
-        return dp[i][j][k]
-    if i==0 and j==0 and k==0:
-        return 0.0
-    
-    res = 0.0
-    if i > 0:
-        res += rec(i-1, j, k) * i
-    if j > 0:
-        res += rec(i+1, j-1, k) * j
-    if k > 0:
-        res += rec(i, j+1, k-1) * k
-    res += N
-    res /= (i + j + k)
+for k in range(three+1):
+    for j in range(two+three+1-k):
+        for i in range(N+1-k-j):
+            if i==0 and j==0 and k==0:
+                continue
 
-    dp[i][j][k] = res
-    return res
+            res = 0.0
+            if i > 0:
+                res += dp[i-1][j][k] * i
+            if j > 0:
+                res += dp[i+1][j-1][k] * j
+            if k > 0:
+                res += dp[i][j+1][k-1] * k
+            res += N
+            res /= (i + j + k)
+            dp[i][j][k] = res
 
-print(rec(one, two, three))
+print(dp[one][two][three])
