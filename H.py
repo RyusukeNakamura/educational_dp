7160a207b4558608df1b7cbc8d79fab950dd1201
RyusukeNakamura <leiden3535@gmail.com>
2021-01-04 09:48:37 +0900

    H use dp

--- H.py
+++ H.py
@@ -1,28 +1,16 @@
-from collections import deque
-
 H, W = map(int, input().split())
 A = [list(input()) for j in range(H)]
 
-deq = deque()
-deq.append((0, 0))
-ans = 0
 mod = 10**9 + 7
+dp = [[0 for j in range(W+1)] for i in range(H+1)]
+dp[1][1] = 1
 
-di = (0, 1)
-dj = (1, 0)
+for i in range(H):
+    for j in range(W):
+        if A[i][j] == "#":
+            continue
 
-for _ in range(10**10):
-    (i, j) = deq.popleft()
+        dp[i+1][j+1] += dp[i][j+1] % mod
+        dp[i+1][j+1] += dp[i+1][j] % mod
 
-    if (i, j) == (H-1, W-1):                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
-        ans += 1
-    
-    for k in range(2):
-        y = i + di[k]
-        x = j + dj[k]
-        
-        if y != H and x != W and A[y][x] != '#':
-            deq.append((y, x))
-    
-    if not deq:
-        break
+print(dp[-1][-1]%mod)
