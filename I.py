c28174850bff69f65a1304852c8477e1c6af4cf4
RyusukeNakamura <leiden3535@gmail.com>
2021-01-04 10:01:34 +0900

    I, use numpy

--- I.py
+++ I.py
@@ -5,7 +5,6 @@ P = list(map(float, input().split()))
 dp = np.zeros((N+1, N+1))
 dp[0][0] = 1
 for i in range(N):
-    for j in range(N):
-        dp[i+1][j+1] += dp[i][j] * P[i]
-        dp[i+1][j] += dp[i][j] * (1 - P[i])
+        dp[i+1][1:] += dp[i][:-1] * P[i]
+        dp[i+1][:-1] += dp[i][:-1] * (1 - P[i])
 print(sum(dp[N][(N+1)//2:]))
