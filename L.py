f78bf58cef2cd4d1c17da47d09ba5c08beb4a843
RyusukeNakamura <leiden3535@gmail.com>
2021-01-05 13:31:29 +0900

    L np cumulative

--- L.py
+++ L.py
@@ -1,20 +1,12 @@
-import sys
-sys.setrecursionlimit(10**8)
+import numpy as np
 N = int(input())
 A = list(map(int, input().split()))
+dp = np.zeros((N+1, N+1), dtype='int64')
+# dp[m][i]: 区間[i,M+i]のときのX-Y
+for m in range(N):
+    if (N - m) % 2 != 0:
+        dp[m+1][:N-m] = np.maximum(dp[m][1:N-m+1] + A[:N-m], dp[m][:N-m] + A[m:])
+    else:
+        dp[m+1][:N-m] = np.minimum(dp[m][1:N-m+1] - A[:N-m], dp[m][:N-m] - A[m:])
 
-dp = [[0 for j in range(N)] for i in range(N)]
-flg = [[False for j in range(N)] for i in range(N)]
-
-def rec(i:int, j:int):
-    if flg[i][j]:
-        return dp[i][j]
-    if i==j:
-        dp[i][j] = A[i]
-        return dp[i][j]
-    flg[i][j] = True
-    # A - rec なのは交互にmax,minが変わるから
-    dp[i][j] = max(A[i] - rec(i+1, j), A[j] - rec(i, j-1))
-    return dp[i][j]
-
-print(rec(0, N-1))
+print(dp[N][0])
