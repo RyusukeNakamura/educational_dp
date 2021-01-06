3784c7720f23afd080c2e0b871bc541b8d85fada
RyusukeNakamura <leiden3535@gmail.com>
2021-01-06 10:59:50 +0900

    M fix for statement

--- M.py
+++ M.py
@@ -1,4 +1,3 @@
-
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
 dp = [[0 for j in range(K+1)] for i in range(N+1)]
@@ -9,7 +8,6 @@ for i in range(N):
     cum[0] = 0
     for j in range(K+1):
         cum[j+1] = (cum[j] + dp[i][j]) % mod
-    for j in range(K+1):
         dp[i+1][j] = (cum[j+1] - cum[max(0, j-A[i])]) % mod
 
 print(dp[N][K])
