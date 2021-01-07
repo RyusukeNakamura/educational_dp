66bf5f1244bce3ae6d61aaac2e2b7dccacd00d24
RyusukeNakamura <leiden3535@gmail.com>
2021-01-08 00:05:25 +0900

    O fix ind

--- O.py
+++ O.py
@@ -5,9 +5,10 @@ dp = [0 for j in range(1 << N)]
 dp[0] = 1
 
 for S in range(1 << N):
+    i = bin(S).count("1")
     for j in range(N):
         # Sのj bit目が1か．
-        if (S >> j) & 1 and A[bin(S).count("1")-1][j]:
+        if (S >> j) & 1 and A[i-1][j]:
             # j番目を固定したときの場合の数．集合Sから要素jを除外．
             dp[S] += dp[S ^ (1 << j)]
             dp[S] %= mod
