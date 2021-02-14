826c7423253f3cece13bd1cd2411cbafcd399625
RyusukeNakamura <leiden3535@gmail.com>
2021-02-14 10:43:32 +0900

    Q

--- Q.py
+++ Q.py
@@ -1,29 +1,21 @@
 N = int(input())
 H = list(map(int, input().split()))
 A = list(map(int, input().split()))
-n = 1 << N.bit_length()
-dat = [0 for i in range(2 * n - 1)]
+bit = [0 for i in range(N + 1)]
 
 def set_value(i, x):
-    i += n - 1 # offset
-    dat[i] = x
-    while i >= 0:
-        if dat[i] < x:
-            dat[i] = x
-        i = (i - 1) // 2
+    while i <= N:
+        bit[i] = max(bit[i], x)
+        i += i & -i
 
 def get_max(i):
-    i += n - 1
-    ret = dat[i]
+    ret = 0
     while i > 0:
-        # iが右の枝なら左の木と比較．
-        if i % 2 == 0:
-            ret = max(ret, dat[i - 1])
-        i = (i - 1) // 2
+        ret = max(ret, bit[i])
+        i -= i & -i
     return ret
 
-# ソートして高さを無視できるように．
-srt = sorted((h, i) for i, h in enumerate(H))
-for h, i in srt:
-    set_value(i, get_max(i) + A[i])
-print(dat[0])
+for i in range(N):
+    mx = get_max(H[i])
+    set_value(H[i], mx + A[i])
+print(max(bit))
